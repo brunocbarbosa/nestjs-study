2168b6b67d6f2d5d26051b754f5a1f400cc3b5c6
brunocbarbosa <bruno.cbarbosa90@gmail.com>
2021-12-13 13:25:06 -0300

    api created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { SequelizeModule } from '@nestjs/sequelize'
+import { join } from 'path';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    OrdersModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      models: [Order],
+      autoLoadModels: true
+    })
+
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
